4b91e241955f9b33373b8a4f5ef706e0682f8f4a
辻気流 <kiryu.18011106@gmail.com>
2020-10-15 07:32:19 +0900

    ToDo 2. checked

--- go/function.go
+++ go/function.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
 	// var a string
 	// fmt.Scan(&a)
 	// fmt.Println("%s", a)
-	stdin := bufio.NewScanner(os.Stdin)
-	stdin.Scan()
-	text := stdin.Text()
-	fmt.Println(text)
+	// stdin := bufio.NewScanner(os.Stdin)
+	// stdin.Scan()
+	// text := stdin.Text()
+	// fmt.Println(text)
+	sosuu()
+}
+
+func sosuu() {
+	for i := 0; i < 20; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
 
 // C:\Users\user\Documents\GitHub\Go-hello-world\go>go run function.go
